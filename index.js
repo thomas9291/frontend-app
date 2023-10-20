e77f072aefd93f970f463440fb0f53ba4fa94a31
thomas <130037065+thomas9291@users.noreply.github.com>
2023-10-20 10:56:02 +0200

    “appfinish“

---
+++ index.js
@@ -0,0 +1,49 @@
+const apiLink = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=a16f15fa2e97f005f287d8849558992e&page=1";
+const img_path = "https://image.tmdb.org/t/p/w500";
+const searchApi = "https://api.themoviedb.org/3/search/movie?api_key=a16f15fa2e97f005f287d8849558992e&query=";
+
+const main = document.getElementById("section");
+const form = document.getElementById("form");
+const search = document.getElementById("query");
+
+returnMovie(apiLink);
+
+function returnMovie(url) {
+    fetch(url).then(res => res.json())
+        .then(function (data) {
+            console.log(data.results);
+            data.results.forEach(element => {
+                const div_card = document.createElement("div");
+                div_card.setAttribute("class", "card");
+                const div_row = document.createElement("div");
+                div_row.setAttribute("class", "row");
+                const div_column = document.createElement("div");
+                div_column.setAttribute("class", "column");
+                const image = document.createElement("img");
+                image.setAttribute("class", "thumbnail");
+                image.setAttribute("id", "image");
+                const title = document.createElement("h3");
+                title.setAttribute("id", "title");
+                const center = document.createElement("center");
+                title.innerHTML = `${element.title}`;
+                image.src = img_path + element.poster_path;
+
+                center.appendChild(image);
+                div_card.appendChild(center);
+                div_column.appendChild(div_card);
+                div_row.appendChild(div_column);
+                main.appendChild(div_row);
+
+            });
+        });
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    main.innerHTML = "";
+    const searchItem = search.value;
+    if (searchItem) {
+        returnMovie(searchApi + searchItem);
+        search.value = "";
+    }
+});
